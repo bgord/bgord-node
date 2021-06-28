cb8e581734f64049d96ea5556a9ba293cf573bb1
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-28 21:51:21 +0200

    feature: add NodeEnvironment schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from "./reporter"
+import * as schema from './schema';
+
+export * from './reporter';
+
+export const Schema = {
+  Port: schema.Port,
+  NodeEnvironment: schema.NodeEnvironment,
+};
---
+++ src/schema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const Port = z
+  .string()
+  .refine(value => !isNaN((value as unknown) as number) && value, {
+    message: 'invalid_port_type',
+  })
+  .transform(value => Number(value))
+  .refine(value => value > 0, { message: 'too_small_port_number' })
+  .refine(value => value < 99999, { message: 'too_big_port_number' });
+
+export enum NodeEnvironmentEnum {
+  local = 'local',
+  test = 'test',
+  staging = 'staging',
+  production = 'production',
+}
+
+export const NodeEnvironment = z.nativeEnum(NodeEnvironmentEnum);
