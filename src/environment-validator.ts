136831d55f6517fda921524f24b7193c436856a3
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-29 19:19:39 +0200

    feature: add environment validator

--- src/environment-validator.ts
+++ src/environment-validator.ts
@@ -1,14 +1,18 @@
-import { ZodSchema } from 'zod';
+import * as z from 'zod';
+import * as dotenv from 'dotenv';
+
+import { Schema } from './index';
+
+type NodeEnvironmentEnumType = z.infer<typeof Schema.NodeEnvironment>;
 
 type EnvironmentFilename = string;
 
-type EnvironmentType = 'local' | 'staging' | 'production' | 'test';
-type AnyZodSchema = ZodSchema<any, any>;
+type AnyZodSchema = z.ZodSchema<any, any>;
 type QuietType = boolean;
 type QuitType = boolean;
 
 type EnvironmentTypeToFilenameType = Record<
-  EnvironmentType,
+  Schema.NodeEnvironmentEnum,
   EnvironmentFilename
 >;
 
@@ -20,26 +24,48 @@ const EnvironmentTypeToFilename: EnvironmentTypeToFilenameType = {
 };
 
 type EnvironmentValidatorConfig = {
-  type: EnvironmentType;
+  type: unknown;
   schema: AnyZodSchema;
   quiet?: QuietType;
   quit?: QuitType;
   environmentTypeToFilename?: EnvironmentTypeToFilenameType;
 };
 
-export class EnvironmentValidator {
-  type: EnvironmentValidatorConfig['type'];
-  schema: EnvironmentValidatorConfig['schema'];
+export class EnvironmentValidator<SchemaType> {
+  type: NodeEnvironmentEnumType;
+  schema: z.Schema<SchemaType>;
   quiet: QuietType;
   quit: QuitType;
   environmentTypeToFilename: EnvironmentTypeToFilenameType;
 
   constructor(config: EnvironmentValidatorConfig) {
-    this.type = config.type;
     this.schema = config.schema;
     this.quiet = config?.quit ?? false;
     this.quit = config?.quit ?? true;
     this.environmentTypeToFilename =
       config?.environmentTypeToFilename ?? EnvironmentTypeToFilename;
+
+    const result = Schema.NodeEnvironment.safeParse(config.type);
+
+    if (result.success) {
+      this.type = result.data;
+    } else {
+      if (this.quit) {
+        console.log(`Invalid EnvironmentType: ${config.type}`);
+        process.exit(1);
+      } else {
+        throw new NodeEnvironmentError();
+      }
+    }
+  }
+
+  load(): SchemaType {
+    const environment = dotenv.config({
+      path: this.environmentTypeToFilename[this.type],
+    }).parsed;
+
+    return this.schema.parse(environment);
   }
 }
+
+class NodeEnvironmentError extends Error {}
