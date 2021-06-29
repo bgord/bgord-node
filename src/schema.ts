7ea77fd26daa6c35bc28c2bb347ed196426e1ae4
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-29 17:50:57 +0200

    feature: add port schema

--- src/schema.ts
+++ src/schema.ts
@@ -6,5 +6,14 @@ export enum NodeEnvironmentEnum {
   staging = 'staging',
   production = 'production',
 }
-
 export const NodeEnvironment = z.nativeEnum(NodeEnvironmentEnum);
+
+// @ts-ignore
+export const Port = z
+  .string()
+  .refine(value => !isNaN((value as unknown) as number) && value, {
+    message: 'invalid_port_type',
+  })
+  .transform(value => Number(value))
+  .refine(value => value > 0, { message: 'too_small_port_number' })
+  .refine(value => value < 99999, { message: 'too_big_port_number' });
