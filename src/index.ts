d964629fb855b2b33378d8081eeea71c429f7508
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-06-29 17:31:31 +0200

    step: remove Port schema for now

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import * as schema from './schema';
 export * from './reporter';
 
 export const Schema = {
-  Port: schema.Port,
   NodeEnvironment: schema.NodeEnvironment,
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,5 @@
 import { z } from 'zod';
 
-export const Port = z
-  .string()
-  .refine(value => !isNaN((value as unknown) as number) && value, {
-    message: 'invalid_port_type',
-  })
-  .transform(value => Number(value))
-  .refine(value => value > 0, { message: 'too_small_port_number' })
-  .refine(value => value < 99999, { message: 'too_big_port_number' });
-
 export enum NodeEnvironmentEnum {
   local = 'local',
   test = 'test',
