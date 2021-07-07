66bbec9f1df4bf0e4a2a29e4d1eee83a49772efa
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-07 16:50:36 +0200

    step: implement handlebars module

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -1,1 +1,18 @@
-export class Handlebars {}
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+export type HandlebarsConfigType = handlebars.ExphbsOptions;
+
+export class Handlebars {
+  config: HandlebarsConfigType;
+  handlebarsDefaultConfig: HandlebarsConfigType = { extname: '.hbs' };
+
+  constructor(config?: HandlebarsConfigType) {
+    this.config = config ?? this.handlebarsDefaultConfig;
+  }
+
+  applyTo(app: express.Application): void {
+    app.engine('handlebars', handlebars.create(this.config).engine);
+    app.set('view engine', 'handlebars');
+  }
+}
