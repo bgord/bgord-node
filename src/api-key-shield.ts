70647da4a071ce7dcce94f7584817bf625e6e080
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-28 13:24:51 +0200

    step: revert wrapping in middleware

--- src/api-key-shield.ts
+++ src/api-key-shield.ts
@@ -1,24 +1,20 @@
 import * as express from 'express';
-import { Middleware } from './middleware';
 
 import { AccessDeniedError } from './errors';
-
 import { ApiKeyType } from './schema';
 
 export class ApiKeyShield {
   static build(apiKey: ApiKeyType) {
-    return Middleware(function verify(
+    return function verify(
       request: express.Request,
       _response: express.Response,
       next: express.NextFunction
     ) {
-      return Middleware(() => {
-        if (request.body.apiKey === apiKey) {
-          return next();
-        }
+      if (request.body.apiKey === apiKey) {
+        return next();
+      }
 
-        throw new AccessDeniedError({ reason: 'api-key' });
-      });
-    });
+      throw new AccessDeniedError({ reason: 'api-key' });
+    };
   }
 }
