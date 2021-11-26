11fc4689c90ce1cc048388d2738aa7c3a5d69620
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-11-26 23:54:12 +0100

    v0.52.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,8 +3,22 @@
 # Preload base bash configuration and functions
 source bgord-scripts/base.sh
 
-info "Make sure all changes are pushed to the master branch"
-press_enter_to_continue
+CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+if test $CURRENT_BRANCH != "master"
+then
+  error "You have to have the master branch checked out"
+  exit 1
+fi
+success "You are on the master branch"
+
+if test $(git rev-parse master) != $(git rev-parse origin/master)
+then
+    error "There are some differences between master and origin/master"
+    info "Please, sync them"
+    exit 1
+fi
+success "All changes are pushed to the remote master branch"
 
 info "Run yarn version --{major,minor,patch}"
 press_enter_to_continue
