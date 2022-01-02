75d676b36fa23c79ed188e42dfc65db1deb9e136
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-02 23:15:25 +0100

    feature: add policy#perform

--- src/policy.ts
+++ src/policy.ts
@@ -1,11 +1,19 @@
 import * as ts from './ts-utils';
 
-export abstract class Policy {
-  abstract fails(...args: any[]): Promise<boolean> | boolean;
+type BasePolicyConfig = Record<string, unknown>;
+
+export abstract class Policy<T extends BasePolicyConfig> {
+  abstract fails(config: T): Promise<boolean> | boolean;
 
   abstract error: ts.Constructor<Error>;
 
   throw() {
     throw new this.error();
+  }
+
+  perform(config: T) {
+    if (this.fails(config)) {
+      this.throw();
+    }
   }
 }
