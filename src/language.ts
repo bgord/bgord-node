28c7b31d6222b8084a732b8940393a3aee17df93
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-03-11 16:40:17 +0100

    feature: read supported language from a directory

--- src/language.ts
+++ src/language.ts
@@ -1,36 +1,53 @@
 import express from 'express';
+import Path from 'path';
+import { promises as fs, PathLike } from 'fs';
 import parser from 'accept-language-parser';
 
 import * as Schema from './schema';
+import { Reporter } from './reporter';
 
 declare global {
   namespace Express {
     export interface Request {
       language: Schema.LanguageNameType;
+      supportedLanguages: Schema.LanguageNameType[];
     }
   }
 }
 
-const defaultLanguageName = 'en';
-
 export class Language {
   static applyTo(
     app: express.Application,
-    supportedLanguageNames: Schema.LanguageNameType[] = [defaultLanguageName]
+    path: PathLike,
+    defaultLanguageName: Schema.LanguageNameType = 'en'
   ): void {
-    app.use((request, _response, next) => {
+    app.use(async (request, _response, next) => {
+      const supportedLanguages = await Language.getSupportedLanguages(path);
+
       const acceptedLanguages = parser.parse(
         request.headers['accept-language']
       );
 
-      const languageName = parser.pick(
-        supportedLanguageNames,
-        acceptedLanguages
-      );
+      const languageName = parser.pick(supportedLanguages, acceptedLanguages);
 
+      request.supportedLanguages = supportedLanguages;
       request.language = languageName ?? defaultLanguageName;
 
       return next();
     });
+  }
+
+  private static async getSupportedLanguages(
+    path: PathLike
+  ): Promise<Schema.LanguageNameType[]> {
+    try {
+      const supportedLanguageFiles = await fs.readdir(path);
+
+      return supportedLanguageFiles.map(filename => Path.parse(filename).name);
+    } catch (error) {
+      Reporter.raw('I18n#build', error);
+
+      return [];
+    }
   }
 }
