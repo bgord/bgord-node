6fbe34fac3397908e541ac4fa087381385c6799d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-03-18 14:05:23 +0100

    step: cleanup unused files

--- japafile.js
+++
@@ -1,9 +0,0 @@
-require('ts-node').register({
-  compilerOptions: {
-    module: 'CommonJS',
-  },
-});
-
-const { configure } = require('japa');
-
-configure({ files: ['test/*.ts'] });
--- test/blah.test.ts
+++
@@ -1,3 +0,0 @@
-import tester from 'japa';
-
-tester('sum', assert => assert.equal(2 + 2, 4));
