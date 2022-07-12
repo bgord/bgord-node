b88a9ffded1c030630cc0634c6d5465f57744a40
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-07-12 20:34:16 +0200

    refactor: export date formatter input type

--- src/dates.ts
+++ src/dates.ts
@@ -2,14 +2,14 @@ import { format, formatDistanceToNow } from 'date-fns';
 
 export type FormattedDateType = string;
 
-type DateType = Parameters<typeof format>[0];
+export type DateFormattersInputType = Parameters<typeof format>[0];
 
 export class DateFormatters {
-  static datetime(date: DateType): FormattedDateType {
+  static datetime(date: DateFormattersInputType): FormattedDateType {
     return format(date, 'yyyy/MM/dd hh:mm');
   }
 
-  static relative(date: DateType) {
+  static relative(date: DateFormattersInputType) {
     return formatDistanceToNow(date, { addSuffix: true });
   }
 }
