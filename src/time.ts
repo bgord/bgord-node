f0a35ca280eeb298d9ec18b8dd8e8d06757cb40d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-03 22:37:27 +0200

    feature: add time converter

---
+++ src/time.ts
@@ -0,0 +1,61 @@
+export class Days {
+  value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  toHours() {
+    return this.value * 24;
+  }
+
+  toMinutes() {
+    return this.value * 24 * 60;
+  }
+
+  toSeconds() {
+    return this.value * 24 * 60 * 60;
+  }
+
+  toMs() {
+    return this.value * 24 * 60 * 60 * 1000;
+  }
+}
+
+export class Hours {
+  value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  toMinutes() {
+    return this.value * 60;
+  }
+
+  toSeconds() {
+    return this.value * 60 * 60;
+  }
+
+  toMs() {
+    return this.value * 60 * 60 * 1000;
+  }
+}
+
+export class Minutes {
+  value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  toSeconds() {
+    return this.value * 60;
+  }
+
+  toMs() {
+    return this.value * 60 * 1000;
+  }
+}
+
+export const Time = { Days, Hours, Minutes };
