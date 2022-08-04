f80f80cbb8e34fc4de995db246e75d424bb445a9
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-04 14:58:19 +0200

    refactor: change time converters to functions

--- src/time.ts
+++ src/time.ts
@@ -1,73 +1,51 @@
-export class Days {
-  value: number;
-
-  constructor(value: number) {
-    this.value = value;
-  }
-
-  toHours() {
-    return this.value * 24;
-  }
-
-  toMinutes() {
-    return this.value * 24 * 60;
-  }
-
-  toSeconds() {
-    return this.value * 24 * 60 * 60;
-  }
-
-  toMs() {
-    return this.value * 24 * 60 * 60 * 1000;
-  }
+export function Days(value: number) {
+  return {
+    toHours() {
+      return value * 24;
+    },
+    toMinutes() {
+      return value * 24 * 60;
+    },
+    toSeconds() {
+      return value * 24 * 60 * 60;
+    },
+    toMs() {
+      return value * 24 * 60 * 60 * 1000;
+    },
+  };
 }
 
-export class Hours {
-  value: number;
-
-  constructor(value: number) {
-    this.value = value;
-  }
-
-  toMinutes() {
-    return this.value * 60;
-  }
-
-  toSeconds() {
-    return this.value * 60 * 60;
-  }
-
-  toMs() {
-    return this.value * 60 * 60 * 1000;
-  }
+export function Hours(value: number) {
+  return {
+    toMinutes() {
+      return value * 60;
+    },
+    toSeconds() {
+      return value * 60 * 60;
+    },
+    toMs() {
+      return value * 60 * 60 * 1000;
+    },
+  };
 }
 
-export class Minutes {
-  value: number;
-
-  constructor(value: number) {
-    this.value = value;
-  }
-
-  toSeconds() {
-    return this.value * 60;
-  }
-
-  toMs() {
-    return this.value * 60 * 1000;
-  }
+export function Minutes(value: number) {
+  return {
+    toSeconds() {
+      return value * 60;
+    },
+    toMs() {
+      return value * 60 * 1000;
+    },
+  };
 }
 
-export class Seconds {
-  value: number;
-
-  constructor(value: number) {
-    this.value = value;
-  }
-
-  toMs() {
-    return this.value * 1000;
-  }
+export function Seconds(value: number) {
+  return {
+    toMs() {
+      return value * 1000;
+    },
+  };
 }
 
 export const Time = { Days, Hours, Minutes, Seconds };
