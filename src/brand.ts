5ee0442ab3e59c34e59e3f47a4bface0c42f367f
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-09-15 18:09:22 +0200

    feature: add weak brand type

--- src/brand.ts
+++ src/brand.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export type Brand<B extends string, T> = { _brand: B } & T;
+export type WeakBrand<B extends string, T> = { _brand?: B } & T;
 
 export function toBrand<T>(schema: z.Schema<unknown>) {
   return schema.transform(x => (x as unknown) as T);
