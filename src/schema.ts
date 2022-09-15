15be515859023422f4c0417a8d6afa1f3156a568
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-09-15 15:34:33 +0200

    refactor: use trim() on VO

--- src/schema.ts
+++ src/schema.ts
@@ -4,6 +4,7 @@ import { Brand, toBrand } from './brand';
 
 export const StringToNumber = z
   .string()
+  .trim()
   .refine(value => !isNaN((value as unknown) as number) && value, {
     message: 'invalid_number',
   })
@@ -21,11 +22,17 @@ export const Port = StringToNumber.refine(value => value > 0, {
   message: 'too_small_port_number',
 }).refine(value => value < 99999, { message: 'too_big_port_number' });
 
-export const HCaptchaSecretKey = z.string().length(42);
+export const HCaptchaSecretKey = z
+  .string()
+  .trim()
+  .length(42);
 
-export const HCaptchaSiteKey = z.string().length(36);
+export const HCaptchaSiteKey = z
+  .string()
+  .trim()
+  .length(36);
 
-export const HCaptchaResponseToken = z.string();
+export const HCaptchaResponseToken = z.string().trim();
 
 export const UUID = z
   .string()
@@ -33,35 +40,55 @@ export const UUID = z
   .default(() => uuid());
 export type UUIDType = z.infer<typeof UUID>;
 
-export const SmtpHost = z.string().min(1);
+export const SmtpHost = z
+  .string()
+  .trim()
+  .min(1);
 export type SmtpHostType = z.infer<typeof SmtpHost>;
 
 export const SmtpPort = Port;
 export type SmtpPortType = z.infer<typeof SmtpPort>;
 
-export const SmtpUser = z.string().min(1);
+export const SmtpUser = z
+  .string()
+  .trim()
+  .min(1);
 export type SmtpUserType = z.infer<typeof SmtpUser>;
 
-export const SmtpPass = z.string().min(1);
+export const SmtpPass = z
+  .string()
+  .trim()
+  .min(1);
 export type SmtpPassType = z.infer<typeof SmtpPass>;
 
 export const Email = z
   .string()
+  .trim()
   .email()
   .min(1);
 export type EmailType = z.infer<typeof Email>;
 
-export const CookieSecret = z.string().length(32);
+export const CookieSecret = z
+  .string()
+  .trim()
+  .length(32);
 export type CookieSecretType = z.infer<typeof CookieSecret>;
 
-export const ContentfulSpaceId = z.string().length(12);
+export const ContentfulSpaceId = z
+  .string()
+  .trim()
+  .length(12);
 export type ContentfulSpaceIdType = z.infer<typeof ContentfulSpaceId>;
 
-export const ContentfulAccessToken = z.string().length(43);
+export const ContentfulAccessToken = z
+  .string()
+  .trim()
+  .length(43);
 export type ContentfulAccessTokenType = z.infer<typeof ContentfulAccessToken>;
 
 export const UrlWithoutTrailingSlash = z
   .string()
+  .trim()
   .url()
   .min(1)
   .refine(value => !value.endsWith('/'), {
@@ -71,31 +98,55 @@ export type UrlWithoutTrailingSlashType = z.infer<
   typeof UrlWithoutTrailingSlash
 >;
 
-export const TwitterApiBearerToken = z.string().length(112);
+export const TwitterApiBearerToken = z
+  .string()
+  .trim()
+  .length(112);
 export type TwitterApiBearerTokenType = z.infer<typeof TwitterApiBearerToken>;
 
-export const ApiKey = z.string().length(64);
+export const ApiKey = z
+  .string()
+  .trim()
+  .length(64);
 export type ApiKeyType = z.infer<typeof ApiKey>;
 
 export const SentryDsn = z.string().url();
 export type SentryDsnType = z.infer<typeof SentryDsn>;
 
-export const TwitterAppKey = z.string().min(1);
+export const TwitterAppKey = z
+  .string()
+  .trim()
+  .min(1);
 export type TwitterAppKeyType = z.infer<typeof TwitterAppKey>;
 
-export const TwitterAppSecret = z.string().min(1);
+export const TwitterAppSecret = z
+  .string()
+  .trim()
+  .min(1);
 export type TwitterAppSecretType = z.infer<typeof TwitterAppSecret>;
 
-export const TwitterAccessToken = z.string().min(1);
+export const TwitterAccessToken = z
+  .string()
+  .trim()
+  .min(1);
 export type TwitterAccessTokenType = z.infer<typeof TwitterAccessToken>;
 
-export const TwitterAccessSecret = z.string().min(1);
+export const TwitterAccessSecret = z
+  .string()
+  .trim()
+  .min(1);
 export type TwitterAccessSecretType = z.infer<typeof TwitterAccessSecret>;
 
-export const RecaptchaSiteKey = z.string().length(40);
+export const RecaptchaSiteKey = z
+  .string()
+  .trim()
+  .length(40);
 export type RecaptchaSiteKeyType = z.infer<typeof RecaptchaSiteKey>;
 
-export const RecaptchaSecretKey = z.string().length(40);
+export const RecaptchaSecretKey = z
+  .string()
+  .trim()
+  .length(40);
 export type RecaptchaSecretKeyType = z.infer<typeof RecaptchaSecretKey>;
 
 export const FeatureFlag = z.literal('yes').or(z.literal('no'));
@@ -104,7 +155,10 @@ export type FeatureFlagType = z.infer<typeof FeatureFlag>;
 export const Localhost = z.literal('127.0.0.1');
 export type LocalhostType = z.infer<typeof Localhost>;
 
-export const MailerliteApiKey = z.string().length(32);
+export const MailerliteApiKey = z
+  .string()
+  .trim()
+  .length(32);
 export type MailerliteApiKeyType = z.infer<typeof MailerliteApiKey>;
 
 export const MailerliteGroupId = z
@@ -118,12 +172,14 @@ export type MailerliteGroupIdType = z.infer<typeof MailerliteGroupId>;
 
 export const AdminUsername = z
   .string()
+  .trim()
   .min(5)
   .max(20);
 export type AdminUsernameType = z.infer<typeof AdminUsername>;
 
 export const AdminPassword = z
   .string()
+  .trim()
   .min(5)
   .max(48);
 export type AdminPasswordType = z.infer<typeof AdminPassword>;
@@ -155,12 +211,16 @@ export const UploadedFile = toBrand<UploadedFileType>(UploadedFileSchema);
 
 export const TimeZoneOffset = z
   .string()
+  .trim()
   .or(z.undefined())
   .transform(value => Number(value))
   .transform(value => (isNaN(value) ? 0 : value));
 
 export type TimeZoneOffsetType = z.infer<typeof TimeZoneOffset>;
 
-export const Language = z.string().default('en');
+export const Language = z
+  .string()
+  .trim()
+  .default('en');
 
 export type LanguageType = z.infer<typeof Language>;
