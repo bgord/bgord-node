87effc0b5a4b2d2e5fab8cd8f37b3e9de906d5da
Bartosz Gordon <gordonbartosz@gmail.com>
2023-03-21 23:20:52 +0100

    feature: add http logger middleware

---
+++ src/http-logger.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import { Logger } from './logger';
+
+import { Middleware } from './middleware';
+
+export class HttpLogger {
+  static applyTo(app: express.Application, logger: Logger): void {
+    app.use(
+      Middleware((request, response, next) => {
+        const client = {
+          ip: request.header('X-Real-IP') ?? request.ip,
+          userAgent: request.header('user-agent'),
+        };
+
+        const httpRequestBeforeMetadata = {
+          params: request.params,
+          headers: request.headers,
+          body: request.body,
+          query: request.query,
+        };
+
+        logger.http({
+          operation: 'http_request_before',
+          requestId: request.requestId,
+          message: 'request',
+          method: request.method,
+          url: `${request.header('host')}${request.url}`,
+          client,
+          metadata: httpRequestBeforeMetadata,
+        });
+
+        response.on('finish', () => {
+          const httpRequestAfterMetadata = {
+            response: response.locals.body,
+          };
+
+          logger.http({
+            operation: 'http_request_after',
+            requestId: request.requestId,
+            message: 'response',
+            method: request.method,
+            url: `${request.header('host')}${request.url}`,
+            responseCode: response.statusCode,
+            client,
+            metadata: httpRequestAfterMetadata,
+          });
+        });
+
+        next();
+      })
+    );
+  }
+}
