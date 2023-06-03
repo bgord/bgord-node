70ae47860a726e9ac24ef5e40b20c19b00044f3d
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-03 15:43:03 +0200

    refactor: don't log some headers

--- src/http-logger.ts
+++ src/http-logger.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import _ from 'lodash';
 
 import { Logger } from './logger';
 import { Middleware } from './middleware';
@@ -12,6 +13,27 @@ export class HttpLogger {
     return JSON.parse(result);
   }
 
+  private static uninformativeHeaders = [
+    'accept',
+    'accept-encoding',
+    'cache-control',
+    'connection',
+    'content-length',
+    'content-type',
+    'cookie',
+    'dnt',
+    'host',
+    'origin',
+    'pragma',
+    'sec-fetch-dest',
+    'sec-fetch-mode',
+    'sec-fetch-site',
+    'sec-fetch-user',
+    'sec-gpc',
+    'upgrade-insecure-requests',
+    'user-agent',
+  ];
+
   static applyTo(app: express.Application, logger: Logger): void {
     app.use(
       Middleware((request, response, next) => {
@@ -22,7 +44,7 @@ export class HttpLogger {
 
         const httpRequestBeforeMetadata = {
           params: request.params,
-          headers: request.headers,
+          headers: _.omit(request.headers, HttpLogger.uninformativeHeaders),
           body: request.body,
           query: request.query,
         };
