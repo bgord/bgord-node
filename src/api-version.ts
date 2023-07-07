870403a9e49ba2e60ce2ed4539f6664dc69d38fa
Bartosz Gordon <gordonbartosz@gmail.com>
2023-07-07 23:42:06 +0200

    feature: add api-version middleware

---
+++ src/api-version.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import { Middleware } from './middleware';
+import { BuildInfoRepository } from './build-info-repository';
+
+export class ApiVersion {
+  static HEADER_NAME = 'api-version';
+
+  static DEFAULT_API_VERSION = 'unknown';
+
+  static throw = Middleware(
+    async (
+      _request: express.Request,
+      response: express.Response,
+      next: express.NextFunction
+    ) => {
+      const build = await BuildInfoRepository.extract();
+
+      response.setHeader(
+        ApiVersion.HEADER_NAME,
+        build.BUILD_VERSION ?? ApiVersion.DEFAULT_API_VERSION
+      );
+
+      return next();
+    }
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * as Errors from './errors';
 export * as Schema from './schema';
 export * as VO from './value-objects';
 export * from './api-key-shield';
+export * from './api-version';
 export * from './basic-auth-shield';
 export * from './build-info-repository';
 export * from './cache'
