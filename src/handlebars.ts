e7b5090bc73f7b802d7f842ccf9cd473af40d34a
Bartosz Gordon <gordonbartosz@gmail.com>
2023-07-22 15:06:52 +0200

    feature: allow for path for handlebars view

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import pluralize from 'pluralize';
+import _ from 'lodash';
 
+import * as Schema from './schema';
 import { DateFormatters } from './dates';
 
-export type HandlebarsConfigType = handlebars.ExphbsOptions;
+export type HandlebarsConfigType = handlebars.ExphbsOptions & {
+  path: Schema.PathType;
+};
 
 export class Handlebars {
   static handlebarsDefaultConfig: HandlebarsConfigType = {
@@ -16,15 +20,17 @@ export class Handlebars {
         count: Parameters<typeof pluralize>[1]
       ) => pluralize(word, count),
     },
+    path: Schema.Path.parse('frontend/views'),
   };
 
   static applyTo(
     app: express.Application,
     _config?: HandlebarsConfigType
   ): void {
-    const config = _config ?? Handlebars.handlebarsDefaultConfig;
+    const config = _.merge(Handlebars.handlebarsDefaultConfig, _config);
 
     app.engine('hbs', handlebars.create(config).engine);
     app.set('view engine', 'hbs');
+    app.set('views', config.path);
   }
 }
