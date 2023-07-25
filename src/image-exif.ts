760d58f3d12ea78a4b42229e566a443f731899a6
Bartosz Gordon <gordonbartosz@gmail.com>
2023-07-25 15:44:26 +0200

    refactor: image exif read method

--- src/image-exif.ts
+++ src/image-exif.ts
@@ -1,37 +1,26 @@
+import sharp from 'sharp';
 import execa from 'execa';
-import { z } from 'zod';
+import path from 'path';
 
 import * as Schema from './schema';
-
-const ImageExifSchema = z.object({
-  image: z.object({
-    geometry: z.object({ width: Schema.Width, height: Schema.Height }),
-    mimeType: z.string().min(1),
-    baseName: z.string().min(1),
-  }),
-});
-type ImageExifSchemaType = z.infer<typeof ImageExifSchema>;
-
-const ImageExifOutput = z.array(ImageExifSchema).nonempty();
 
 export type ImageExifOutputType = {
   width: Schema.WidthType;
   height: Schema.HeightType;
-  mimeType: ImageExifSchemaType['image']['mimeType'];
-  name: ImageExifSchemaType['image']['baseName'];
+  name: path.ParsedPath['base'];
 };
 
 export class ImageEXIF {
-  static async read(path: Schema.PathType): Promise<ImageExifOutputType> {
-    const { stdout } = await execa('magick', ['convert', path, 'json:']);
+  static async read(input: Schema.PathType): Promise<ImageExifOutputType> {
+    const image = sharp(input);
+    const metadata = await image.metadata();
 
-    const output = ImageExifOutput.parse(JSON.parse(stdout));
+    const name = path.parse(input).base;
 
     return {
-      width: output[0].image.geometry.width,
-      height: output[0].image.geometry.height,
-      mimeType: output[0].image.mimeType,
-      name: output[0].image.baseName,
+      width: Schema.Width.parse(metadata.width),
+      height: Schema.Height.parse(metadata.height),
+      name,
     };
   }
 
