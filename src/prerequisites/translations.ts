c9cce2b2f827fc099c5562e8cdd9a030111d6163
Bartosz Gordon <gordonbartosz@gmail.com>
2023-08-07 18:02:36 +0200

    feature: add translations diff prereq

--- src/prerequisites/translations.ts
+++ src/prerequisites/translations.ts
@@ -1,7 +1,8 @@
 import fs from 'fs/promises';
 import { constants } from 'fs';
 
-import { I18n, I18nConfigType } from '../i18n';
+import * as Schema from '../schema';
+import { I18n, I18nConfigType, TranslationsKeyType } from '../i18n';
 import {
   PrerequisiteLabelType,
   PrerequisiteStrategyEnum,
@@ -13,6 +14,12 @@ export type PrerequisiteTranslationsStrategyConfigType = {
   strategy: PrerequisiteStrategyEnum.translations;
   translationsPath?: typeof I18n.DEFAULT_TRANSLATIONS_PATH;
   supportedLanguages: I18nConfigType['supportedLanguages'];
+};
+
+type PrerequisiteTranslationsProblemType = {
+  translationKey: TranslationsKeyType;
+  existsInLanguage: Schema.LanguageType;
+  missingInLanguage: Schema.LanguageType;
 };
 
 export class PrerequisiteTranslationsVerificator {
@@ -35,6 +42,50 @@ export class PrerequisiteTranslationsVerificator {
       return PrerequisiteStatusEnum.failure;
     }
 
-    return PrerequisiteStatusEnum.success;
+    const supportedLanguages = Object.keys(config.supportedLanguages);
+
+    if (supportedLanguages.length === 1) return PrerequisiteStatusEnum.success;
+
+    const languageToTranslationKeys: Record<
+      Schema.LanguageType,
+      TranslationsKeyType[]
+    > = {};
+
+    const problems: PrerequisiteTranslationsProblemType[] = [];
+
+    for (const language of supportedLanguages) {
+      const translations = I18n.getTranslations(language, translationsPath);
+      const translationKeys = Object.keys(translations);
+
+      languageToTranslationKeys[language] = translationKeys;
+    }
+
+    for (const currentLanguage in languageToTranslationKeys) {
+      const translationKeys = languageToTranslationKeys[currentLanguage] ?? [];
+
+      for (const translationKey of translationKeys) {
+        for (const supportedLanguage of supportedLanguages) {
+          if (supportedLanguage === currentLanguage) continue;
+
+          const translationKeyExists = languageToTranslationKeys[
+            supportedLanguage
+          ]?.some(key => translationKey === key);
+
+          if (!translationKeyExists) {
+            problems.push({
+              translationKey,
+              existsInLanguage: currentLanguage,
+              missingInLanguage: supportedLanguage,
+            });
+          }
+        }
+      }
+    }
+
+    if (problems.length === 0) return PrerequisiteStatusEnum.success;
+
+    console.log(problems);
+
+    return PrerequisiteStatusEnum.failure;
   }
 }
