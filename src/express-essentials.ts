2ea7bfcffa9ef36172f99e9b462a40ff2197dbfa
Bartosz Gordon <gordonbartosz@gmail.com>
2023-08-08 12:14:43 +0200

    fix: img-src helmet config

--- src/express-essentials.ts
+++ src/express-essentials.ts
@@ -48,6 +48,7 @@ export function addExpressEssentials(
             "'self'",
             ...HCaptchaShield.helmetCspConfig['connect-src'],
           ],
+          'img-src': ["'self'", 'data:', 'blob:'],
         },
       },
     },
