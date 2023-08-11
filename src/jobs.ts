5f0d6be8b333852a7a057d04bf3e4c0db4b8d569
Bartosz Gordon <gordonbartosz@gmail.com>
2023-08-11 21:22:20 +0200

    step: add stopAll

--- src/jobs.ts
+++ src/jobs.ts
@@ -1,3 +1,7 @@
 export class Jobs {
   static SCHEDULES = { EVERY_MINUTE: '* * * * *' };
+
+  static stopAll(jobs: Record<string, { stop: VoidFunction }>) {
+    Object.values(jobs).forEach(job => job.stop());
+  }
 }
