7845f3f9df1f2b781c1f9d25065089cd94572d32
Bartosz Gordon <gordonbartosz@gmail.com>
2023-08-26 15:40:31 +0200

    step: add healthcheck durationMs

--- src/healthcheck.ts
+++ src/healthcheck.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { Prerequisite, PrerequisiteStatusEnum } from './prerequisites';
+import { Stopwatch } from './stopwatch';
 import { Middleware } from './middleware';
 
 export class Healthcheck {
@@ -10,6 +11,8 @@ export class Healthcheck {
       response: express.Response,
       _next: express.NextFunction
     ) {
+      const stopwatch = new Stopwatch();
+
       const details = [];
 
       for (const prerequisite of prerequisites) {
@@ -26,7 +29,7 @@ export class Healthcheck {
 
       const code = ok ? 200 : 424;
 
-      return response.status(code).send({ ok, details });
+      return response.status(code).send({ ok, details, ...stopwatch.stop() });
     }
 
     return Middleware(handle);
