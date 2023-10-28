d23019bae3adc6be12f43d898aadc1f59b27b2c4
Bartosz Gordon <gordonbartosz@gmail.com>
2023-10-28 13:45:46 +0200

    feature: add download file class

---
+++ src/download-file.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import fs from 'fs';
+
+import { Mime } from './mime';
+
+export type DownloadFileConfigType = { filename: fs.PathLike; mime: Mime };
+
+export class DownloadFile {
+  public static attach(
+    response: express.Response,
+    config: DownloadFileConfigType
+  ) {
+    response.setHeader(
+      'Content-Disposition',
+      `attachment; filename="${config.filename}"`
+    );
+    response.setHeader('Content-Type', config.mime.raw);
+  }
+}
