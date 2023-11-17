08e6ff37773a1c9020819ca0f239019690d90388
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-17 22:21:48 +0100

    refactor: update etag

--- src/etag.ts
+++ src/etag.ts
@@ -1,55 +1,141 @@
-import * as z from 'zod';
+import express from 'express';
 
-export const Revision = z.coerce
-  .number()
-  .int()
-  .positive();
-export type RevisionType = z.infer<typeof Revision>;
+export type RevisionValueType = number;
+
+export class InvalidRevisionError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, InvalidRevisionError.prototype);
+  }
+}
+
+export class RevisionMismatchError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, RevisionMismatchError.prototype);
+  }
+}
+
+export class Revision {
+  readonly value: RevisionValueType;
+
+  constructor(value: unknown) {
+    if (!Number.isInteger(value) || typeof value !== 'number' || value < 0) {
+      throw new InvalidRevisionError();
+    }
+
+    this.value = value;
+  }
+
+  matches(another: RevisionValueType): boolean {
+    return this.value === another;
+  }
+
+  validate(another: RevisionValueType): void {
+    if (!this.matches(another)) {
+      throw new RevisionMismatchError();
+    }
+  }
+
+  increment(): Revision {
+    return new Revision(this.value + 1);
+  }
+
+  static fromETag(etag: ETag | null): Revision {
+    if (!etag) {
+      throw new InvalidRevisionError();
+    }
+    return new Revision(etag.revision);
+  }
+
+  static fromWeakETag(weakEtag: WeakETag | null): Revision {
+    if (!weakEtag) {
+      throw new InvalidRevisionError();
+    }
+    return new Revision(weakEtag.revision);
+  }
+}
 
 export type ETagValueType = string;
 
 export class ETag {
+  static HEADER_NAME = 'ETag';
+
+  static IF_MATCH_HEADER_NAME = 'if-match';
+
   readonly value: ETagValueType;
 
-  constructor(readonly revision: RevisionType) {
+  private constructor(readonly revision: RevisionValueType) {
     this.value = revision.toString();
   }
 
-  matches(another: ETag): boolean {
-    return this.value === another.value && this.revision === another.revision;
-  }
+  static fromHeader(value?: ETagValueType): ETag | null {
+    if (value?.startsWith('W/')) return null;
 
-  static fromValue(value: ETagValueType): ETag {
-    if (value.startsWith('W/')) {
-      throw Error('WeakETag value supplied for a strong ETag constructor');
-    }
-    const revision = Revision.parse(value);
-
-    return new ETag(revision);
+    return new ETag(Number(value));
   }
 }
 
 export type WeakETagValueType = string;
 
 export class WeakETag {
+  static HEADER_NAME = 'ETag';
+
+  static IF_MATCH_HEADER_NAME = 'if-match';
+
   readonly value: WeakETagValueType;
 
-  constructor(readonly revision: RevisionType) {
+  private constructor(readonly revision: RevisionValueType) {
     this.value = `W/${revision.toString()}`;
   }
 
-  matches(another: WeakETag): boolean {
-    return this.value === another.value && this.revision === another.revision;
-  }
-
-  static fromValue(value: WeakETagValueType): WeakETag {
+  static fromHeader(value: WeakETagValueType): WeakETag {
     if (!value.startsWith('W/')) {
-      throw Error('Strong ETag value supplied for a Weak ETag constructor');
+      throw Error('Invalid WeakETag');
     }
 
-    const candidate = value.split('W/')[1];
-    const revision = Revision.parse(candidate);
+    const candidate = Number(value.split('W/')[1]);
+    return new WeakETag(candidate);
+  }
+}
 
-    return new WeakETag(revision);
+declare global {
+  namespace Express {
+    export interface Request {
+      ETag: ETag | null;
+      WeakETag: WeakETag | null;
+    }
+  }
+}
+
+export class ETagExtractor {
+  static applyTo(app: express.Application): void {
+    app.use(async (request, _response, next) => {
+      try {
+        const header = String(request.headers[ETag.IF_MATCH_HEADER_NAME]);
+        const etag = ETag.fromHeader(header);
+        request.ETag = etag;
+      } catch (error) {
+        request.ETag = null;
+      }
+
+      next();
+    });
+  }
+}
+
+export class WeakETagExtractor {
+  static applyTo(app: express.Application): void {
+    app.use(async (request, _response, next) => {
+      try {
+        const header = String(request.headers[WeakETag.IF_MATCH_HEADER_NAME]);
+        const weakEtag = WeakETag.fromHeader(header);
+        request.WeakETag = weakEtag;
+      } catch (error) {
+        request.WeakETag = null;
+      }
+
+      next();
+    });
   }
 }
--- tests/etag.test.ts
+++ tests/etag.test.ts
@@ -1,64 +1,114 @@
 import { describe, test, expect } from 'vitest';
-import { ETag, RevisionType, WeakETag } from '../src/etag';
+import {
+  ETag,
+  WeakETag,
+  InvalidRevisionError,
+  Revision,
+  RevisionMismatchError,
+} from '../src/etag';
 
-describe('ETag', () => {
-  test('ETag constructor should create a valid ETag instance', () => {
-    const revision: RevisionType = 123;
-    const eTag = new ETag(revision);
+describe('Revision class', () => {
+  test('Revision constructor should create a valid Revision instance', () => {
+    const revisionValue = 123;
+    const revision = new Revision(revisionValue);
 
-    expect(eTag.value).toBe(revision.toString());
-    expect(eTag.revision).toBe(revision);
+    expect(revision.value).toBe(revisionValue);
   });
 
-  test('ETag fromValue should create a valid ETag instance', () => {
-    const value = '123';
-    const eTag = ETag.fromValue(value);
+  test('Revision constructor should throw InvalidRevisionError for invalid input', () => {
+    const invalidValue = 'invalid';
 
-    expect(eTag.value).toBe(value);
-    expect(eTag.revision).toBe(123); // Assuming the revision value is 123
+    expect(() => new Revision(invalidValue)).toThrowError(InvalidRevisionError);
   });
 
-  test('ETag fromValue should throw an error for WeakETag value', () => {
-    const weakETagValue = 'W/123';
+  test('Revision matches should correctly compare two Revision instances', () => {
+    const revision1 = new Revision(123);
+    const revision2 = new Revision(123);
+    const revision3 = new Revision(456);
 
-    expect(() => ETag.fromValue(weakETagValue)).toThrowError(
-      'WeakETag value supplied for a strong ETag constructor'
+    expect(revision1.matches(revision2.value)).toBe(true);
+    expect(revision1.matches(revision3.value)).toBe(false);
+  });
+
+  test('Revision validate should throw RevisionMismatchError for mismatched revisions', () => {
+    const revision1 = new Revision(123);
+    const revision2 = new Revision(456);
+
+    expect(() => revision1.validate(revision2.value)).toThrowError(
+      RevisionMismatchError
     );
   });
 
-  test('ETag matches should correctly compare two ETag instances', () => {
-    const eTag1 = new ETag(123);
-    const eTag2 = new ETag(123);
-    const eTag3 = new ETag(456);
+  test('Revision increment should create a new Revision with incremented value', () => {
+    const revision = new Revision(123);
+    const incrementedRevision = revision.increment();
 
-    expect(eTag1.matches(eTag2)).toBe(true);
-    expect(eTag1.matches(eTag3)).toBe(false);
+    expect(incrementedRevision.value).toBe(revision.value + 1);
+  });
+
+  test('Revision fromETag should create a valid Revision instance from ETag', () => {
+    const etag = ETag.fromHeader('123');
+    const revision = Revision.fromETag(etag);
+
+    // @ts-expect-error
+    expect(revision.value).toBe(etag.revision);
+  });
+
+  test('Revision fromETag should throw InvalidRevisionError for null ETag', () => {
+    const etag = null;
+
+    expect(() => Revision.fromETag(etag)).toThrowError(InvalidRevisionError);
+  });
+
+  test('Revision fromWeakETag should create a valid Revision instance from WeakETag', () => {
+    const weakEtag = WeakETag.fromHeader('W/123');
+    const revision = Revision.fromWeakETag(weakEtag);
+
+    expect(revision.value).toBe(weakEtag.revision);
+  });
+
+  test('Revision fromWeakETag should throw InvalidRevisionError for null WeakETag', () => {
+    const weakEtag = null;
+
+    expect(() => Revision.fromWeakETag(weakEtag)).toThrowError(
+      InvalidRevisionError
+    );
+  });
+});
+
+describe('ETag class', () => {
+  test('ETag fromHeader should create a valid ETag instance', () => {
+    const value = '123';
+    const etag = ETag.fromHeader(value);
+
+    //@ts-expect-error
+    expect(etag.value).toBe(value);
+    //@ts-expect-error
+    expect(etag.revision).toBe(123);
+  });
+
+  test('ETag fromHeader should return null for WeakETag value', () => {
+    const weakETagValue = 'W/123';
+    const etag = ETag.fromHeader(weakETagValue);
+
+    expect(etag).toBe(null);
   });
 });
 
 describe('WeakETag class', () => {
-  test('WeakETag constructor should create a valid WeakETag instance', () => {
-    const revision: RevisionType = 123;
-    const weakETag = new WeakETag(revision);
-
-    expect(weakETag.value).toBe(`W/${revision}`);
-    expect(weakETag.revision).toBe(revision);
-  });
-
-  test('WeakETag fromValue should create a valid WeakETag instance', () => {
+  test('WeakETag fromHeader should create a valid WeakETag instance', () => {
     const value = 'W/123';
-    const weakETag = WeakETag.fromValue(value);
+    const weakEtag = WeakETag.fromHeader(value);
 
-    expect(weakETag.value).toBe(value);
-    expect(weakETag.revision).toBe(123); // Assuming the revision value is 123
+    expect(weakEtag.value).toBe(value);
+    expect(weakEtag.revision).toBe(123);
   });
 
-  test('WeakETag matches should correctly compare two WeakETag instances', () => {
-    const weakETag1 = new WeakETag(123);
-    const weakETag2 = new WeakETag(123);
-    const weakETag3 = new WeakETag(456);
+  test('WeakETag fromHeader should throw an error for invalid WeakETag value', () => {
+    const invalidValue = 'invalid';
 
-    expect(weakETag1.matches(weakETag2)).toBe(true);
-    expect(weakETag1.matches(weakETag3)).toBe(false);
+    expect(() => WeakETag.fromHeader(invalidValue)).toThrowError(
+      'Invalid WeakETag'
+    );
   });
 });
