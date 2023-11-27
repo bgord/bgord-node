bf6a1a5143ec02cb92c60c7c9516bfe6c0b2cbac
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-27 16:48:24 +0100

    step: slower middleware

--- src/slower.ts
+++ src/slower.ts
@@ -5,13 +5,13 @@ import { Middleware } from './middleware';
 type SlowerConfigType = { ms?: number };
 
 export class Slower {
-  static build(config: SlowerConfigType) {
+  static build(config?: SlowerConfigType) {
     async function handle(
       _request: express.Request,
       _response: express.Response,
       next: express.NextFunction
     ) {
-      const ms = config.ms ?? 500;
+      const ms = config?.ms ?? 500;
       await sleep({ ms });
 
       next();
---
+++ tests/slower.test.ts
@@ -0,0 +1,72 @@
+import express from 'express';
+import { describe, test, expect } from 'vitest';
+import request from 'supertest';
+
+import { Stopwatch } from '../src/stopwatch';
+import { Slower } from '../src/slower';
+
+describe('Slower middleware', () => {
+  test('works with default configuration', async () => {
+    const app = express();
+
+    app.get('/ping', Slower.build(), (_request, response) =>
+      response.status(200).send('pong')
+    );
+
+    const stopwatch = new Stopwatch();
+
+    await request(app)
+      .get('/ping')
+      .expect(200);
+
+    expect(stopwatch.stop().durationMs).toBeGreaterThanOrEqual(500);
+  });
+
+  test('works with custom configuration', async () => {
+    const app = express();
+
+    app.get('/ping', Slower.build({ ms: 50 }), (_request, response) =>
+      response.status(200).send('pong')
+    );
+
+    const stopwatch = new Stopwatch();
+    await request(app)
+      .get('/ping')
+      .expect(200);
+
+    expect(stopwatch.stop().durationMs).toBeGreaterThanOrEqual(50);
+  });
+
+  test('works with zero milliseconds configuration', async () => {
+    const app = express();
+
+    app.get('/ping', Slower.build({ ms: 0 }), (_request, response) =>
+      response.status(200).send('pong')
+    );
+
+    const stopwatch = new Stopwatch();
+    await request(app)
+      .get('/ping')
+      .expect(200);
+
+    expect(stopwatch.stop().durationMs).toBeGreaterThanOrEqual(0);
+  });
+
+  test('works with multiple middlewares', async () => {
+    const app = express();
+
+    app.get(
+      '/ping',
+      Slower.build({ ms: 50 }),
+      (_request, _response, next) => next(),
+      (_request, response) => response.status(200).send('pong')
+    );
+
+    const stopwatch = new Stopwatch();
+    await request(app)
+      .get('/ping')
+      .expect(200);
+
+    expect(stopwatch.stop().durationMs).toBeGreaterThanOrEqual(50);
+  });
+});
