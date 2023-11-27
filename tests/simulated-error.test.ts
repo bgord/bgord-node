9075d2734fe51686bdada7f8dc02d43500bcbdf9
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-27 16:50:06 +0100

    step: simulated error tests

---
+++ tests/simulated-error.test.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { describe, test, expect } from 'vitest';
+import request from 'supertest';
+import { SimulatedError } from '../src/simulated-error';
+
+describe('SimulatedError middleware', () => {
+  test('throws an error', async () => {
+    const app = express();
+
+    app.get('/ping', SimulatedError.throw, (_request, _response) => {});
+
+    const result = await request(app)
+      .get('/ping')
+      .expect(500);
+
+    expect(result.text).toContain('Simulated error');
+  });
+
+  test('works with multiple middlewares', async () => {
+    const app = express();
+
+    app.get(
+      '/ping',
+      (_request, _response, next) => next(),
+      SimulatedError.throw,
+      (_request, _response) => {}
+    );
+
+    const result = await request(app)
+      .get('/ping')
+      .expect(500);
+
+    expect(result.text).toContain('Simulated error');
+  });
+});
