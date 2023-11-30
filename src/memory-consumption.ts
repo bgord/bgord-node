0f2dd61621c672583562664d70b5ca311465b220
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-30 15:02:52 +0100

    step: add memory consumption test

--- src/memory-consumption.ts
+++ src/memory-consumption.ts
@@ -1,9 +1,7 @@
 import { Size, SizeUnit } from './size';
 
-export type MemoryConsumptionResultType = Size;
-
 export class MemoryConsumption {
-  static get(): MemoryConsumptionResultType {
+  static get(): Size {
     const memoryConsumption = process.memoryUsage().rss;
 
     return new Size({ value: memoryConsumption, unit: SizeUnit.b });
---
+++ tests/memory-consumption.test.ts
@@ -0,0 +1,22 @@
+import { describe, test, expect, vi } from 'vitest';
+
+import { MemoryConsumption } from '../src/memory-consumption';
+import { SizeUnit } from '../src/size';
+
+describe('MemoryConsumption class', () => {
+  test('get method returns memory consumption size', () => {
+    const spy = vi.spyOn(process, 'memoryUsage');
+
+    spy.mockImplementationOnce(() => ({
+      ...process.memoryUsage(),
+      rss: 123123123,
+    }));
+
+    const result = MemoryConsumption.get();
+
+    expect(result.toBytes()).toBe(123123123);
+    expect(result.format(SizeUnit.MB)).toBe('117.42 MB');
+
+    spy.mockRestore();
+  });
+});
