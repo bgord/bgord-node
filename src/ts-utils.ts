99a2015d29477f3dd5a8a614edc5f5e82e5b266e
Bartosz Gordon <gordonbartosz@gmail.com>
2024-08-23 12:21:02 +0200

    step: remove async return type

--- src/ts-utils.ts
+++ src/ts-utils.ts
@@ -1,13 +1,5 @@
 export type Constructor<T> = new (...args: any[]) => T;
 
-export type AsyncReturnType<T extends (...args: any) => any> = T extends (
-  ...args: any
-) => Promise<infer U>
-  ? U
-  : T extends (...args: any) => infer U
-  ? U
-  : any;
-
 export type Falsy<T> = T | null | undefined;
 
 export type Nullable<T> = T | null;
