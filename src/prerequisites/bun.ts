4d9ff3cdc6e96c1b9952f8374de81f55a95f1747
Bartosz Gordon <gordonbartosz@gmail.com>
2024-10-13 00:32:29 +0200

    step: allow bun debug

--- src/prerequisites/bun.ts
+++ src/prerequisites/bun.ts
@@ -22,14 +22,20 @@ export class PrerequisiteBun extends AbstractPrerequisite<PrerequisiteBunConfigT
   }
 
   async verify(): Promise<PrerequisiteStatusEnum> {
+    console.log(this.config);
     if (!this.enabled) return PrerequisiteStatusEnum.undetermined;
+    console.log("enabled");
 
     const { stdout } = await $`bun -v`;
+    console.log(stdout);
     const current = PackageVersion.fromString(stdout);
+    console.log(current);
 
     if (current.isGreaterThanOrEqual(this.config.version)) {
+      console.log("passed");
       return this.pass();
     }
+    console.log("rejected");
     return this.reject();
   }
 }
