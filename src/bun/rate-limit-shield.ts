40c6e36cf1fe438a5a130b3aca68b938c9e875a4
Bartosz Gordon <gordonbartosz@gmail.com>
2024-10-12 19:23:21 +0200

    fix: rateLimitShield

--- src/bun/rate-limit-shield.ts
+++ src/bun/rate-limit-shield.ts
@@ -1,5 +1,4 @@
-import { Env } from "hono";
-import { createMiddleware } from "hono/factory";
+import { MiddlewareHandler } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 import * as Schema from "../schema";
@@ -11,12 +10,12 @@ export const TooManyRequestsError = new HTTPException(429, {
   message: "app.too_many_requests",
 });
 
-export function rateLimitShield<E extends Env = Env>(
+export const rateLimitShield = (
   options: RateLimitShieldOptionsType
-) {
+): MiddlewareHandler => {
   const rateLimiter = new RateLimiter(options);
 
-  return createMiddleware<E>(async (_c, next) => {
+  return async function requestId(_c, next) {
     const currentTimestampMs = Date.now();
     const check = rateLimiter.verify(currentTimestampMs);
 
@@ -25,5 +24,5 @@ export function rateLimitShield<E extends Env = Env>(
     }
 
     return next();
-  });
-}
+  };
+};
